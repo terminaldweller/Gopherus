7fa59db2891bc9cfbbd07efee5e09b39da787b48
terminaldweller <devi@terminaldweller.com>
2024-11-05 00:53:49 +0000

    python3

--- gopherus.py
+++ gopherus.py
@@ -1,31 +1,49 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 import argparse
 import sys
-sys.path.insert(0,'./scripts/')
-from scripts import FastCGI, MySQL, PostgreSQL, DumpMemcached, PHPMemcached, PyMemcached, RbMemcached, Redis, SMTP, Zabbix
+
+sys.path.insert(0, "./scripts/")
+from scripts import (
+    FastCGI,
+    MySQL,
+    PostgreSQL,
+    DumpMemcached,
+    PHPMemcached,
+    PyMemcached,
+    RbMemcached,
+    Redis,
+    SMTP,
+    Zabbix,
+)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--exploit",
-                        help="mysql,\n"
-                             "postgresql,\n"
-                             "fastcgi,\n"
-                             "redis,\n"
-                             "smtp,\n"
-                             "zabbix,\n"
-                             "pymemcache,\n"
-                             "rbmemcache,\n"
-                             "phpmemcache,\n"
-                             "dmpmemcache")
+parser.add_argument(
+    "--exploit",
+    help="mysql,\n"
+    "postgresql,\n"
+    "fastcgi,\n"
+    "redis,\n"
+    "smtp,\n"
+    "zabbix,\n"
+    "pymemcache,\n"
+    "rbmemcache,\n"
+    "phpmemcache,\n"
+    "dmpmemcache",
+)
 args = parser.parse_args()
 
-class colors:
-    reset='\033[0m'
-    red='\033[31m'
-    green='\033[32m'
-    orange='\033[33m'
-    blue='\033[34m'
 
-print colors.green + """
+class colors:
+    reset = "\033[0m"
+    red = "\033[31m"
+    green = "\033[32m"
+    orange = "\033[33m"
+    blue = "\033[34m"
+
+
+print(
+    colors.green
+    + """
 
   ________              .__
  /  _____/  ____ ______ |  |__   ___________ __ __  ______
@@ -33,31 +51,39 @@ print colors.green + """
 \    \_\  (  <_> )  |_> >   Y  \  ___/|  | \/  |  /\___ \\
  \______  /\____/|   __/|___|  /\___  >__|  |____//____  >
         \/       |__|        \/     \/                 \/
-""" + "\n\t\t" + colors.blue + "author: " + colors.orange + "$_SpyD3r_$" + "\n" + colors.reset
+"""
+    + "\n\t\t"
+    + colors.blue
+    + "author: "
+    + colors.orange
+    + "$_SpyD3r_$"
+    + "\n"
+    + colors.reset
+)
 
-if(not args.exploit):
-    print parser.print_help()
+if not args.exploit:
+    print(parser.print_help())
     exit()
 
-if(args.exploit=="mysql"):
+if args.exploit == "mysql":
     MySQL.MySQL()
-elif(args.exploit=="postgresql"):
+elif args.exploit == "postgresql":
     PostgreSQL.PostgreSQL()
-elif(args.exploit=="fastcgi"):
+elif args.exploit == "fastcgi":
     FastCGI.FastCGI()
-elif(args.exploit=="redis"):
+elif args.exploit == "redis":
     Redis.Redis()
-elif(args.exploit=="smtp"):
+elif args.exploit == "smtp":
     SMTP.SMTP()
-elif(args.exploit=="zabbix"):
+elif args.exploit == "zabbix":
     Zabbix.Zabbix()
-elif(args.exploit=="dmpmemcache"):
+elif args.exploit == "dmpmemcache":
     DumpMemcached.DumpMemcached()
-elif(args.exploit=="phpmemcache"):
+elif args.exploit == "phpmemcache":
     PHPMemcached.PHPMemcached()
-elif(args.exploit=="rbmemcache"):
+elif args.exploit == "rbmemcache":
     RbMemcached.RbMemcached()
-elif(args.exploit=="pymemcache"):
+elif args.exploit == "pymemcache":
     PyMemcached.PyMemcached()
 else:
-    print parser.print_help()
+    print(parser.print_help())
